bb79d2592726ddb7c9af303ca4e05f2102c676a7
outcastzzz <fleshka4895@gmail.com>
2024-01-15 01:13:20 +0300

    add interceptor

--- app/src/main/java/com/example/cryptoapp/data/network/ApiFactory.kt
+++ app/src/main/java/com/example/cryptoapp/data/network/ApiFactory.kt
@@ -1,11 +1,24 @@
 package com.example.cryptoapp.data.network
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiFactory {
 
     private const val BASE_URL = "https://min-api.cryptocompare.com/data/"
+
+    private fun setupInterceptor(): OkHttpClient {
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+
+        return OkHttpClient.Builder()
+            .addInterceptor(interceptor)
+            .build()
+    }
+
+    private val client = setupInterceptor()
 
     private val retrofit = Retrofit.Builder()
         .addConverterFactory(GsonConverterFactory.create())
